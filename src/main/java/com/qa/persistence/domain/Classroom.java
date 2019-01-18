273f1597823c22cd440ea7da0a9685df17a8308b
OussamaMlouk <o.mlouk@hotmail.co.uk>
2019-01-18 10:31:31 +0000

    Created trainee and classroom entities.

---
+++ src/main/java/com/qa/persistence/domain/Classroom.java
@@ -0,0 +1,52 @@
+package com.qa.persistence.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Classroom {
+	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	private Long classroomID;
+	private String trainer;
+	private Trainee trainees;
+	
+	public Classroom() {
+		
+	}
+	
+	public Classroom (String trainer, Trainee trainees) {
+		this.trainer = trainer;
+		this.trainees = trainees;
+	}
+
+	public Long getClassroomID() {
+		return classroomID;
+	}
+
+	public void setClassroomID(Long classroomID) {
+		this.classroomID = classroomID;
+	}
+
+	public String getTrainer() {
+		return trainer;
+	}
+
+	public void setTrainer(String trainer) {
+		this.trainer = trainer;
+	}
+
+	public Trainee getTrainees() {
+		return trainees;
+	}
+
+	public void setTrainees(Trainee trainees) {
+		this.trainees = trainees;
+	}
+	
+	
+
+}
