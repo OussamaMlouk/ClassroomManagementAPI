39f30ac7bc0a53da713e40b5ea58cd9393944401
OussamaMlouk <o.mlouk@hotmail.co.uk>
2019-01-18 12:23:32 +0000

    Added Service implementation for Classroom.

---
+++ src/main/java/com/qa/business/service/ClassroomServiceImpl.java
@@ -0,0 +1,37 @@
+package com.qa.business.service;
+
+import javax.inject.Inject;
+
+import com.qa.persistence.domain.Classroom;
+import com.qa.persistence.repository.ClassroomRepository;
+
+public class ClassroomServiceImpl implements ClassroomService {
+
+	@Inject
+	private ClassroomRepository repo;
+	
+	public String getAllClassrooms() {
+		return repo.getAllClassrooms();
+	}
+
+	public String createClassroom(String classroom) {
+		return repo.createClassroom(classroom);
+	}
+
+	public Classroom findClassroom(Long classroomID) {
+		return repo.findClassroom(classroomID);
+	}
+
+	public String updateClassroom(String classroom, Long classroomID) {
+		return repo.updateClassroom(classroom, classroomID);
+	}
+
+	public String deleteClassroom(Long classroomID) {
+		return repo.deleteClassroom(classroomID);
+	}
+	
+	public void setRepo(ClassroomRepository repo) {
+		this.repo = repo;
+	}
+
+}
