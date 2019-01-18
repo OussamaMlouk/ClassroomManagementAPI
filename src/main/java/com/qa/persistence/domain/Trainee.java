161acd4b523206c9aa297c80b39ff18f13f8ec44
OussamaMlouk <o.mlouk@hotmail.co.uk>
2019-01-18 11:32:37 +0000

    Added classroomID to trainee entity.

--- src/main/java/com/qa/persistence/domain/Trainee.java
+++ src/main/java/com/qa/persistence/domain/Trainee.java
@@ -11,14 +11,23 @@ public class Trainee {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Id
 	private Long traineeID;
+	private Long classroomID;
 	private String traineeName;
 
 	public Trainee() {
-
 	}
 
 	public Trainee(String traineeName) {
 		this.traineeName = traineeName;
+		this.classroomID = null;
+	}
+
+	public Long getClassroomID() {
+		return classroomID;
+	}
+
+	public void setClassroomID(Long classroomID) {
+		this.classroomID = classroomID;
 	}
 
 	public String getTraineeName() {
